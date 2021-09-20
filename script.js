b6506976ea6476fc8f15b13009ed02053517c097
Nuanjan <nuanjan.schluntz@gmail.com>
2021-09-20 13:55:14 -0400

    Add more card and apply click function for each element.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var like = document.querySelector("#like");
-var count = 0;
-function increaseLike() {
-  count++;
-  like.innerText = count;
+// var like = document.querySelector("#like");
+var count1 = 0,
+  count2 = 0,
+  count3 = 0;
+function increaseLike(el) {
+  if (el === like1) {
+    count1++;
+    like1.innerText = count1;
+  } else if (el === like2) {
+    count2++;
+    like2.innerText = count2;
+  } else {
+    count3++;
+    like3.innerText = count3;
+  }
 }
